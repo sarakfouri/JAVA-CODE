07591fdd759fa7b98966c63eab05b312dba9b7ab
Sara Kfouri <sakfouri@gmail.com>
2017-10-30 22:11:54 +0000

    new
    new

---
+++ Variables.java
@@ -0,0 +1,11 @@
+package com.java21;
+
+public class Variables {
+public static void main (String args[]){
+final  char UP = 'u';
+short location = 13250;
+int score = 3500100;
+boolean newGame = true;
+
+}
+}
