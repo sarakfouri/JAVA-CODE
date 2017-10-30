d033a7c494db057d985b5cd8c2e45930b682132e
Sara Kfouri <sakfouri@gmail.com>
2017-10-30 23:33:19 +0000

    updload
    upload

---
+++ RefTester.java
@@ -0,0 +1,16 @@
+package com.java21;
+import java.awt.Point;
+public class RefTester {
+	public static void main (String[] args){
+		Point pt1 ,  pt2;
+		pt1 = new Point(100, 100);
+		pt2 = pt1;
+		
+		pt1.x = 200;
+		pt2.y = 200;
+		System.out.println("Point1: " + pt1.x +", " + pt1.y);
+		pt2.y = 200;
+		System.out.println("Point1: " + pt2.x + ", " + pt2.y);
+	}
+
+}
